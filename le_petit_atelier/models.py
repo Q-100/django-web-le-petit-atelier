5108607b8e2f5548739d1e17c0c8fe4d42b545f8
Q-100 <opea5954@gmail.com>
2021-02-23 07:14:27 +0900

    migrations 수정

--- le_petit_atelier/models.py
+++ le_petit_atelier/models.py
@@ -7,8 +7,11 @@ class Question(models.Model):
     content = models.TextField()  # 본문(글자수 제한없음)
     create_date = models.DateTimeField()  # 날짜
 
+    def __str__(self):
+        return self.subject
+
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) # 어떤 질문에 대한 답변(다른 모델을 속성으로 가지면 ForeignKey를 사용)
-    content = models.TextField() # 본문
-    create_date = models.DateTimeField() # 날짜
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # 어떤 질문에 대한 답변(다른 모델을 속성으로 가지면 ForeignKey를 사용)
+    content = models.TextField()  # 본문
+    create_date = models.DateTimeField()  # 날짜
