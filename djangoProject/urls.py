1b021e0a464e9f11294cb3a4308f4c90a958e419
Q-100 <opea5954@gmail.com>
2021-02-22 19:27:54 +0900

    start app

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from le_petit_atelier import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('le_petit_atelier/', views.index),
 ]
---
+++ le_petit_atelier/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LePetitAtelierConfig(AppConfig):
+    name = 'le_petit_atelier'
