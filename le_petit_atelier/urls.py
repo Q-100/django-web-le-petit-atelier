4d5b54cbfddf5ea4161bc75e9b6c50edb1d81875
kyuback <opea5954@gmail.com>
2021-08-12 17:09:39 +0900

    github new func

--- le_petit_atelier/urls.py
+++ le_petit_atelier/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path('le_petit_atelier/', include('le_petit_atelier.urls')),로 처리했기때문에
     # 여기의 urls에 path('le_petit_atelier/'로하면 결국
     # le_petit_atelier/le_petit_atelier/ 가 되기 때문에 앞에서 처리한걸 뒤에서 또할필요없음
+    # test
     path('', views.index, name='index'),
     path("<int:question_id>", views.detail, name='detail'),
     path("answer/create/<int:question_id>", views.answer_create, name="answer_create"),
